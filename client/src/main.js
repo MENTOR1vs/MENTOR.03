5006c1979ff5dc0a9636844d2f9f7d1cd04bf655
Alexandra009mm <alexaaoerozco@gmail.com>
2026-07-14 15:53:57 -0500

    Delete client directory

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-import { ApiService } from "./services/ApiService.js";
-import { AppRouter } from "./router.js";
-
-const root = document.querySelector("#app");
-const api = new ApiService("/api");
-const router = new AppRouter({ root, api });
-
-router.start();
