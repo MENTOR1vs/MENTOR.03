43138015dd0995b1b448919e767fa2e7725bfb2c
valery <valejo0310@gmail.com>
2026-07-20 20:13:36 -0500

    docs

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     host: "0.0.0.0", // Allows access from the host computer when Vite runs in docker
     port: Number(process.env.PORT || 5173),
     strictPort: true, //Stops starup instead of selecting another port
-    proxy: { //Keeps API urls relative in the frontend
+    proxy: { //Keeps API urls relative in the frontend.
       "/api": {
         target:
           process.env.VITE_API_PROXY_TARGET || "http://localhost:3000",
